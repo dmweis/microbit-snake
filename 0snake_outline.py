cf8817e67657c337b6cb0fe7164432f5b02bba5c
David Weis <davidmichaelweis@gmail.com>
2019-10-23 15:07:22 +0100

    Update basic skeleton for prewired

--- 0snake_outline.py
+++ 0snake_outline.py
@@ -1,4 +1,12 @@
 from microbit import *
+from random import randint
+
+def random_point():
+    """ This function returns a random point
+        on the playing board
+    """
+    return [randint(0, 4), randint(0, 4)]
+
 
 class Snake:
     """ This class contains the functions that operate
@@ -14,13 +22,13 @@ class Snake:
         """
         ## UNCOMMENT AND FILL IN THE # LINES BELOW WITH START VALUES
         ## current direction is a string with up, down, left or right
-        # self.current_direction =
+        self.current_direction = "up"
         ## snake is a list of the pixels that the snake is at
-        # self.snake = [[2, 2]]
+        self.snake = [[2, 2]]
         ## food is the co-ords of the current food
-        # self.food =
+        self.food = random_point()
         ## whether or not to end the game, used after update
-        # self.end =
+        self.end = False
         pass
 
     def handle_input(self):
@@ -28,7 +36,19 @@ class Snake:
             user to control which direction the snake is going
             in.
         """
-        pass
+        x = accelerometer.get_x()
+        y = accelerometer.get_y()
+        # abs is the absolute function eg -1 => 1, 1 => 1
+        if abs(x) > abs(y):
+            if x < 0:
+                self.current_direction = "left"
+            else:
+                self.current_direction = "right"
+        else:
+            if y < 0:
+                self.current_direction = "up"
+            else:
+                self.current_direction = "down"
 
     def update(self):
         """ This function will update the game state
@@ -36,24 +56,28 @@ class Snake:
         """
         # The line below makes a copy of the head of the snake
         # you will be working with that copy in this function
-        # cur_head = list(self.snake[-1])
-        pass
+        new_head = list(self.snake[-1])
+
 
     def draw(self):
         """ This makes the game appear on the LEDs. """
         display.clear()
-        display.set_pixel(self.food[0], self.food[1], 5)
+        if self.food[0] >= 0 and self.food[0] <= 4 and self.food[1] >= 0 and self.food[1] <= 4:
+            display.set_pixel(self.food[0], self.food[1], 5)
         for part in self.snake:
-            display.set_pixel(part[0], part[1], 9)
+            if part[0] >= 0 and part[0] <= 4 and part[1] >= 0 and part[1] <= 4:
+                display.set_pixel(part[0], part[1], 9)
 
 # game is an "instance" of Snake
 game = Snake()
 
 # this is called our "game loop" and is where everything
 # happens
-while True:
+while not game.end:
     game.handle_input()
     game.update()
     game.draw()
     # this makes our micro:bit do nothing for 500ms
     sleep(500)
+
+display.show("Game over")
